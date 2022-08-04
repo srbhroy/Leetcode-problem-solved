3ef2206ed62e36a7300ba14014ed893e24007711
Sourabh Roy <srbhroy5@gmail.com>
2022-08-04 23:58:41 +0530

    Added optimised solution for problem_167

--- Two Pointers/problem_167.cpp
+++ Two Pointers/problem_167.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> result;
-        for(int i = 0; i < numbers.size(); i++) {
-            int remainder = target - numbers[i];
-            for(int j = i + 1; j < numbers.size() && remainder >= numbers[j]; j++) {
-                if(remainder == numbers[j]) {
-                    result.push_back(i+1);
-                    result.push_back(j+1);
+         vector<int> result(2,0);
+        int j = numbers.size() - 1;
+        for(int i = 0;i<numbers.size();) {
+                int sum = numbers[i] + numbers[j];
+                if(j < 0)   break;
+                if(sum == target) {
+                    result[0] = ++i;
+                    result[1] = ++j;
                     return result;
                 }
-            }
+                sum < target?++i:--j;
         }
         return result;
     }
