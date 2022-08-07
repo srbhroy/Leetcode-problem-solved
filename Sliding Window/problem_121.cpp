6c90baa561fc0f8037793116ed3da5019ad8ce17
Sourabh Roy <srbhroy5@gmail.com>
2022-08-07 23:54:44 +0530

    Added optimised solution for problem#121

--- Sliding Window/problem_121.cpp
+++ Sliding Window/problem_121.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        for(int i = 0; i < prices.size(); i++)    {
-            for(int j = i + 1; j < prices.size(); j++)    {
-                int remainder = prices[j] - prices[i];
-                if(remainder > profit)
-                    profit = remainder;
-            }
+        int min = prices[0];
+        for(int i = 1; i < prices.size(); i++)    {
+            if(prices[i] < min) min = prices[i];
+            int remainder = prices[i] - min;
+            profit = max(profit, remainder);
         }
         return profit;
     }
