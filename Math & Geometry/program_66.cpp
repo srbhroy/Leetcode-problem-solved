e2fcfcf379e7152e387bc4ee5a8e653313f474f7
Sourabh Roy <srbhroy5@gmail.com>
2022-09-07 01:02:37 +0530

    Added optimised solution for program#66.

--- Math & Geometry/program_66.cpp
+++ Math & Geometry/program_66.cpp
@@ -1,24 +1,21 @@
 class Solution {
-	public:	
-	vector<int> plusOne(vector<int>& digits) {
-        int i = digits.size() - 1, j = 0;
-        int sum = digits[i] + 1;
-        int carry = sum / 10;
-
-        while(digits[i] + carry > 9 && i >= 0)    {
-            digits[i] = 0;
-            --i;
-            if(i >= 0)  {
-                sum = digits[i] + 1;
-                carry = sum / 10;
-                continue;
-            }
-            if(carry == 1 && i < 0) {
-                digits.insert(digits.begin(), 1);
-                return digits;
-            }
-        }
-        digits[i] += 1;
-        return digits;
+public:
+    vector<int> plusOne(vector<int>& digits) {
+     int n = digits.size() - 1;
+     int carry = 0;
+     while(n >= 0)   {
+         if(digits[n] < 9)  {
+             digits[n] += 1;
+             carry = 0;
+             break;
+         }
+         else   {
+             digits[n] = 0;
+             carry = 1;
+         }
+         --n;
+     }
+    if(carry) digits.insert(digits.begin(),1);
+    return digits;
     }
 };
