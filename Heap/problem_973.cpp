a4a415eb66d11229aa36dc3f4e45d8985fb87674
Sourabh Roy <srbhroy5@gmail.com>
2022-09-09 22:42:04 +0530

    Added optimised solution to problem#973.

--- Heap/problem_973.cpp
+++ Heap/problem_973.cpp
@@ -1,44 +1,26 @@
-class Solution {
+class cmp {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int low = 0;
-        int high = points.size() - 1;
-        int pivotIndex = points.size();
-        
-        while (pivotIndex != k) {
-            pivotIndex = partition(points, low, high);
-            if (pivotIndex < k) {
-                low = pivotIndex;
-            } else {
-                high = pivotIndex - 1;
-            }
-        }
-        
-        return vector<vector<int>>(points.begin(), points.begin() + k);
-    }
-private:
-    int partition(vector<vector<int>>& points, int low, int high) {
-        vector<int> pivot = points[low + (high - low) / 2];
-        int pivotDistance = getDistance(pivot);
-        
-        while (low < high) {
-            if (getDistance(points[low]) >= pivotDistance) {
-                swap(points[low], points[high]);
-                high--;
-            } else {
-                low++;
-            }
-        }
-        
-        if (getDistance(points[low]) < pivotDistance) {
-            low++;
-        }
-        return low;
-    }
-    
-    int getDistance(vector<int>& point) {
-        return pow(point[0], 2) + pow(point[1], 2);
+        bool operator()(pair<int,pair<int,int>> a, pair<int,pair<int,int>> b)    {
+        return a.first > b.first;
     }
 };
-
-
+class Solution {
+public:
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    vector<vector<int>> result(k);
+    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,cmp> hash;
+    for(auto i : points)    {
+        int distance = (pow(i[0],2) + pow(i[1],2));
+        hash.push(make_pair(distance,make_pair(i[0],i[1])));
+    }
+    while(k)  {
+        auto i = hash.top().second.first ,j = hash.top().second.second;
+        result[k - 1].push_back(i);
+        result[k - 1].push_back(j);
+        hash.pop();
+        --k;
+    }
+    return result;
+   }
+};
