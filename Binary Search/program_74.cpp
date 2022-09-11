e414e5e9857660f06c0a2a96e5f0936482d95409
Sourabh Roy <srbhroy5@gmail.com>
2022-09-11 23:31:54 +0530

    Added optimised solution for program_74.

--- Binary Search/program_74.cpp
+++ Binary Search/program_74.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int lowRow = 0;
-        int highRow = matrix.size() - 1;
-        
-        while (lowRow < highRow) {
-            int mid = lowRow + (highRow - lowRow) / 2;
-            if (matrix[mid][0] == target) {
-                return true;
-            }
-            if (matrix[mid][0] < target && target < matrix[mid + 1][0]) {
-                lowRow = mid;
-                break;
-            }
-            if (matrix[mid][0] < target) {
-                lowRow = mid + 1;
-            } else {
-                highRow = mid - 1;
-            }
+        int j = 0;
+    int i = matrix.size() - 1, siz = matrix[0].size() - 1, mid_column;
+    while(j <= i)   {
+        mid_column = j + ((i-j)/2);
+        if(target >= matrix[mid_column][0] && target <= matrix[mid_column][siz])    {
+            j = mid_column;
+            break;
         }
-        
-        int lowCol = 0;
-        int highCol = matrix[0].size() - 1;
-        
-        while (lowCol <= highCol) {
-            int mid = lowCol + (highCol - lowCol) / 2;
-            if (matrix[lowRow][mid] == target) {
-                return true;
-            }
-            if (matrix[lowRow][mid] < target) {
-                lowCol = mid + 1;
-            } else {
-                highCol = mid - 1;
-            }
+        else if(target > matrix[mid_column][siz])    j = mid_column + 1;
+        else    i = mid_column - 1;
+    }
+    if(j == matrix.size())   return false;
+
+        int l = 0, r = siz, mid;
+        while(l <= r)   {
+            mid = l + ((r-l)/2);
+            if(matrix[j][mid] == target)    return true;
+            else if(target < matrix[j][mid])    r = mid - 1;
+            else    l = mid + 1;
         }
-        
         return false;
     }
 };
