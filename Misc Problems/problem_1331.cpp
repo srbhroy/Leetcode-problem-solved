ccd2d6b63938bd1399704d1fcb9b231425ca4c3e
Sourabh Roy <srbhroy5@gmail.com>
2022-10-20 09:16:25 +0530

    Added optimised solution to problem#1331.

--- Misc Problems/problem_1331.cpp
+++ Misc Problems/problem_1331.cpp
@@ -4,14 +4,19 @@ public:
         vector<int> res = arr;
         sort(res.begin(),res.end());
         map<int,int> r;
+        int flag = 0;
+        int count = 0;
         for(int i = 0; i < arr.size(); i++) {
             int data = res[i];
-            if(r.find(data) == r.end()) r[data] = i;
+            if(r.find(data) == r.end()) {
+                ++count;
+                r[data] = count;
+            }
         }
         vector<int> result;
         for(int i : arr) {
             int data = r[i];
-            result.push_back(data + 1);
+            result.push_back(data);
         }
         return result;
     }
