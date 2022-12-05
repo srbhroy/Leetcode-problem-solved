bbce190e51d7a8d5204ea7c28162e5b18b138489
Sourabh Roy <srbhroy5@gmail.com>
2022-12-05 22:14:30 +0530

    Added optimised solution to problem#905.

--- Misc Problems/problem_905.cpp
+++ Misc Problems/problem_905.cpp
@@ -1,13 +1,18 @@
-class Solution {
-public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        list<int> temp;
-        vector<int> result;
-        for(int i : nums)
-            if(i % 2 == 0)  temp.push_front(i);
-            else temp.push_back(i);
-        for(int i : temp)
-            result.push_back(i);
-        return result;
-    }
-};
+vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1;
+        while(i < j)    {
+            int odd = nums[i] % 2;
+            int even = nums[j] % 2;
+            if(odd && !even) {
+                nums[i] ^= nums[j];
+                nums[j] ^= nums[i];
+                nums[i] ^= nums[j];
+                --j;
+                ++i;
+            }
+            else if(odd)    --j;
+            else ++i;
+            
+        }
+        return nums;
+}
