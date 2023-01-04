865212883cd2c60b4a3db819e07dca8afb944e0e
Sourabh Roy <srbhroy5@gmail.com>
2023-01-04 23:48:51 +0530

    Added modified solution to problem#424.

--- Sliding Window/problem_424.cpp
+++ Sliding Window/problem_424.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(26);
-        int maxCount = 0;
-        
-        int i = 0;
-        int j = 0;
-        
-        int result = 0;
-        
-        while (j < s.size()) {
-            count[s[j] - 'A']++;
-            maxCount = max(maxCount, count[s[j] - 'A']);
-            if (j - i + 1 - maxCount > k) {
-                count[s[i] - 'A']--;
-                i++;
+        int l = 0, r = s.size() - 1, mid;
+        while(l <= r)   {
+            mid = l + (r - l) / 2;
+            bool ans = false;
+            unordered_map<char,int> countt;
+            for(int i = 0; i < s.size(); i++)   {
+                ++countt[s[i]];
+                if(i > mid) {
+                    --countt[s[i - mid - 1]];
+                }
+                unordered_map<char,int>::iterator best = max_element(countt.begin(),countt.end(),[] (const std::pair<char,     int>& a, const std::pair<char,int>& b)->bool{ return a.second < b.second; } );
+                if(mid + 1 - best->second <= k) {
+                    ans = true;
+                    break;
+                }
             }
-            result = max(result, j - i + 1);
-            j++;
+            if(ans) {
+                l = mid + 1;
+            }
+            else r = mid - 1;
         }
-        
-        return result;
+        return l;
     }
 };
